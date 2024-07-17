7894936b322cc21ce7d03b83f65cde3b1bc7fee9
David <david.navas@tecnologicoliceocristiano.edu.ec>
2024-07-16 21:22:26 -0500

    suma participacion en clase

---
+++ suma.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+primernumero = int(input("Escribe el primer numero: "))
+segundonumero = int(input("Escribe el segundo numero: "))
+
+suma = primernumero + segundonumero
+print(suma)
+
+tercernumero = int(input("Escribe el tercer numero: "))
+
+multi = suma * tercernumero
+print(multi)
