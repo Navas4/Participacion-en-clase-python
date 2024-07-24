1fb4f87cbfa14537a6efa98f4e7cf6603b01af3b
David <david.navas@tecnologicoliceocristiano.edu.ec>
2024-07-23 20:01:12 -0500

    participacioncompleta

---
+++ Porcentaje.py
@@ -0,0 +1,12 @@
+# Escribe un programa que solicite al usuario
+# un numero y le reste el 15%, almacenando
+# todo en una unica variable. A continuacion,
+# mostrar el resultado final en pantalla.
+ 
+numero = int(input("Escribe un numero: "))
+print(numero - (numero*15)/100)
+
+
+
+
+ 
