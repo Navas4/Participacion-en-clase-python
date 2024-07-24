ee2afc96033a99388f6fbee45d95c95e8a8935b8
David <david.navas@tecnologicoliceocristiano.edu.ec>
2024-07-23 20:13:58 -0500

    participacionenclase

---
+++ Concatenar.py
@@ -0,0 +1,10 @@
+# Escriba un programa que solicite al usuario el ingreso de dos palabras,
+# las cuales se guardaran en dos variables distintas.
+# A continuacion, almacena en una variable la concatenacion de la primera palabra,
+# mas un espacio, mas la segunda palabra. Muestre el resultado en pantalla.
+
+p1 = input("ingresar la primera palabra: ")
+p2 = input("ingresar la segunda palabra: ")
+p3 = p1 +" "+ p2
+print(p3)
+
